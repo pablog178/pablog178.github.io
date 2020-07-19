4aa71ba737324c7100632900adb7c746d13bd8b3
Pablo Guevara <pablog.178@gmail.com>
2020-07-18 22:33:24 -0400

    Update footer style

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,7 +58,7 @@ const Layout = ({ location, title, children }) => {
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
+      <footer style={{ marginTop: 100 }}>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
